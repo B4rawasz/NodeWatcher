3f86bf057ee8f0e46599cf2b44040a4b235ef137
B4rawasz <69699459+B4rawasz@users.noreply.github.com>
2026-02-10 17:23:32 +0100

    Refactor UiTaskBroker to unify logger variable and improve requestTask method signature

--- packages/electron/src/modules/task_broker/task_broker.ts
+++ packages/electron/src/modules/task_broker/task_broker.ts
@@ -1,9 +1,10 @@
+import { v4 as uuidv4 } from "uuid";
 import type { UiTaskRes, UiTaskReq, UiTaskType, AnyUiTaskReq } from "@nodewatcher/shared";
 import { logger } from "../logger/logger.js";
 
 export class UiTaskBroker {
 	private static instance: UiTaskBroker;
-	private brokerLogger = logger.registerModule("UiTaskBroker");
+	private logger = logger.registerModule("UiTaskBroker");
 
 	private pendingTasks = new Map<
 		string,
@@ -20,7 +21,7 @@ export class UiTaskBroker {
 		this.sendTaskRequest = sendTaskRequest;
 	}
 
-	static getInstance(sendTaskRequest: (req: AnyUiTaskReq) => void): UiTaskBroker {
+	static getInstance(sendTaskRequest?: (req: AnyUiTaskReq) => void): UiTaskBroker {
 		if (!UiTaskBroker.instance) {
 			if (!sendTaskRequest) {
 				throw new Error("UiTaskBroker instance not initialized and no sendTaskRequest function provided");
@@ -30,8 +31,21 @@ export class UiTaskBroker {
 		return UiTaskBroker.instance;
 	}
 
-	requestTask<K extends UiTaskType>(req: UiTaskReq<K>, timeout = 300000): Promise<UiTaskRes<K>["payload"]> {
-		this.brokerLogger.verbose(`Requesting task of type ${req.type} with id ${req.id}`, req);
+	requestTask<K extends UiTaskType>(
+		type: K,
+		payload: UiTaskReq<K>["payload"],
+		source: string,
+		timeout = 300000,
+	): Promise<UiTaskRes<K>["payload"]> {
+		const req: UiTaskReq<K> = {
+			id: uuidv4(),
+			type,
+			payload,
+			source,
+			createdAt: Date.now(),
+		};
+
+		this.logger.verbose(`Requesting task of type ${req.type} with id ${req.id}`, req);
 		return new Promise((resolve, reject) => {
 			const timeoutId = setTimeout(() => {
 				this.pendingTasks.delete(req.id);
@@ -44,14 +58,14 @@ export class UiTaskBroker {
 	}
 
 	handleTaskResponse<K extends UiTaskType>(res: UiTaskRes<K>) {
-		this.brokerLogger.verbose(`Handling task response for task id ${res.id} of type ${res.type}`, res);
+		this.logger.verbose(`Handling task response for task id ${res.id} of type ${res.type}`, res);
 		const pending = this.pendingTasks.get(res.id);
 		if (pending) {
 			clearTimeout(pending.timeout);
 			pending.resolve(res.payload);
 			this.pendingTasks.delete(res.id);
 		} else {
-			this.brokerLogger.warn(`No pending task found for response with id ${res.id}`);
+			this.logger.warn(`No pending task found for response with id ${res.id}`);
 		}
 	}
 }
